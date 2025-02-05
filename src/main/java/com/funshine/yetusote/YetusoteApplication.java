55b4ec4642416d6d3d05be6d2a47886797b3e7fc
Funshine <funshine@localhost.localdomain>
2025-02-05 16:55:03 +0300

    Initial commit, configured database connection

---
+++ src/main/java/com/funshine/yetusote/YetusoteApplication.java
@@ -0,0 +1,17 @@
+package com.funshine.yetusote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableMongoRepositories(basePackages = "com.funshine.yetusote.repository")
+@EnableMongoAuditing
+@SpringBootApplication
+public class YetusoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetusoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funshine/yetusote/YetusoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funshine.yetusote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetusoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
