c2dffe7e6cff7797ad7ead6646edded4710e670d
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2025-03-14 12:37:21 +0300

    handled cors'

--- src/main/java/com/funshine/yetusote/controller/LoanController.java
+++ src/main/java/com/funshine/yetusote/controller/LoanController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1/loans")
 @Tag(name ="Loan", description = "Loan API's")
